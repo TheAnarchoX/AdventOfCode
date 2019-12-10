c70942d5f8f1a88712fab11bf7bf4b5305133db9
Jim de Vries <j.devries@resoftware.nl>
2019-12-10 17:47:33 +0100

    Day 1 finished

--- aoc.py
+++ aoc.py
@@ -5,17 +5,11 @@ import requests
 @click.option('-y', '--year', prompt='Which year of AoC would you like to run?', type=click.IntRange(2015, 2019))
 @click.option('-d', '--day', prompt='And which day?', type=click.IntRange(1,25))
 def main(year, day):
-
-    runAoC(f"year{year}.day{day}.solve", "solve")(input=get_input(year, day))
-    pass
+    runAoC(f"year{year}.day{day}.solve", "solve")()
 
 def runAoC(module, name):
     module = __import__(module, fromlist=[name])
     return getattr(module, name)
-
-def get_input(year, day):
-    # TODO WRITE INPUT Requests
-    pass
 
 if __name__ == '__main__':
     main()
--- year2019/day1/solve.py
+++ year2019/day1/solve.py
@@ -1,2 +1,23 @@
+import os
+from math import floor
+input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
+
 def solve():
-    print("Solving y2019:d1")
+    data = [int(module) for module in open(input_path, 'r').read().split('\n')]
+
+    result = 0
+
+    for module in data:
+        result = result + getFuel(module)
+
+    print(result)
+
+def getFuel(module):
+    res = 0
+    fuel = module
+    while fuel > 0:
+        fuel = ((floor(fuel/3)) - 2)
+        if fuel > 0:
+            res += fuel
+
+    return res
