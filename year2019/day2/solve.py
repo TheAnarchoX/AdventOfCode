5439a3a9f5ce6027e937ea0c47580675c634587f
Jim de Vries <j.devries@resoftware.nl>
2019-12-11 10:51:34 +0100

    Improved multiprocessing

--- year2019/day2/solve.py
+++ year2019/day2/solve.py
@@ -1,55 +1,54 @@
+""" 2019 Day 2 Solver"""
 import os
 import sys
-from multiprocessing import Pool, Manager
+from typing import Tuple, List
+from multiprocessing import Manager, Process
+from multiprocessing.managers import ValueProxy
 
-input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
+INPUT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input.txt")
 
 
 def solve():
-    pool = Pool(5)
-
+    """ Solve https://adventofcode.com/2019/day/2"""
     print("Generating map")
-    map = [[x, y] for x in range(0, 1000) for y in range(0, 1000)]
+    value_map = [(x, y) for x in range(0, 100) for y in range(0, 100)]
+    data = [int(module) for module in open(INPUT_PATH, 'r').read().split(',')]
 
-    print("RUNNING")
-    pool.map(run_intcode, map, 10)
+    finish = Manager().Value(bool, False)
+    procs = []
+    for tile in value_map:
+        procs.append(Process(target=run_intcode, args=(data, tile, finish)))
+
+    print(f'Starting {len(procs)} subprocesses')
+    for proc in procs:
+        if not finish.get():
+            proc.start()
+            proc.join()
 
     sys.exit()
 
 
-def run_intcode(params):
-    x, y = params
+def run_intcode(data: List[int], tile: Tuple[int, int], finish: ValueProxy):
+    """ This runs intcode base on a data list a tile from the value map
+        :param data
+        :returns void
+    """
+    initial_noun, initial_verb = tile
 
-    data = [int(module) for module in open(input_path, 'r').read().split(',')]
-    data[1] = x
-    data[2] = y
+    data[1] = initial_noun
+    data[2] = initial_verb
 
     for i in range(0, len(data) - 1, 4):
+        if finish.get():
+            sys.exit()
         opcode, noun, verb, out = data[i:i + 4]
 
         if opcode == 1:
-            try:
-                data[out] = data[noun] + data[verb]
-            except IndexError:
-                break
+            data[out] = data[noun] + data[verb]
         elif opcode == 2:
-            try:
-                data[out] = data[noun] * data[verb]
-            except IndexError:
-                break
+            data[out] = data[noun] * data[verb]
         elif opcode == 99:
-            if (data[0] == 19_690_720):
-                info(f'X: {x}')
-                info(f'Y: {y}')
-                info(f'Result: {100 * x + y}')
-
-                import signal
-
-                os.kill(os.getppid(), signal.SIGTERM)
-            else:
-                break
-
-
-def info(title):
-    print(title)
-
+            if data[0] == 19690720:
+                print(f'Result: {100 * initial_noun + initial_verb}')
+                finish.set(True)
+                sys.exit()
